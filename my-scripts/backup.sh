a12547628bad61f2b25fa1b0d6d06ccd17f2a575
jane <jql1377219787@gmail.com>
2020-03-27 09:40:47 +0800

    config custom themes

--- my-scripts/backup.sh
+++ my-scripts/backup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-cp ~/.zshrc ~/OpenSource/my-linux/zsh &
-echo "backup .zshrc"
-cp -r ~/.config/nvim/* ~/OpenSource/my-linux/config/nvim &
+cp ~/.zshrc ../zsh/zshrc &
+cp ~/.zimrc ../zsh/zimrc &
+echo "backup zsh-config"
+cp -r ~/.config/nvim/* ../config/nvim &
 echo "backup nvim-config"
-cp -r ~/.config/ranger/* ~/OpenSource/my-linux/config/ranger &
+cp -r ~/.config/ranger/* ../config/ranger &
 echo "back up ranger-config"
-cp ~/.gvimrc ~/OpenSource/my-linux/jane &
-echo "back up MacVim-config"
-cp ~/.myclirc ~/OpenSource/my-linux/jane &
-echo "back up mycli-config"
-cp /etc/hosts ~/OpenSource/my-linux/etc &
+cp /etc/hosts ../etc/hosts &
 echo "back up hosts"
-cp ~/.ipython/profile_default/ipython_config.py
-echo "back up ipython config"
+cp ~/.ipython/profile_default/ipython_config.py ../ipython/profile_default/ipython_config.py
+echo "back up ipython-config"
 echo "Done"
 
