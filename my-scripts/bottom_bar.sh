952fe9344353e2036dcadb2c69090d4edd20ce82
jane42070 <1377219787@qq.com>
2020-03-30 23:03:27 +0800

    update scripts

--- my-scripts/bottom_bar.sh
+++ my-scripts/bottom_bar.sh
@@ -1,21 +1,20 @@
-#!/usr/bin/env bash
-
-
+#!/bin/bash
 # Dynamic Window Manager Status Bar Config
 # 1.Show date time
 # 2.Show battery status info
-while true; do
-		xsetroot -name " | Dis.$(df -h | awk '{ if ($6 == "/") print $4 }') | Mem.$(free -h | awk '(NR==2){ print $3 }') | Bat.$(acpi -b|awk '{print $4}') | Vol.$(amixer get Master|tail -n 1|awk '{print $5}'|tr -d '[]') | $(date '+%Y-%m-%d %R ')"
-		# Update time
-		sleep 0.2s
-	done &
-# # 3.Show audio info
-# while true;
-# do
-#    if acpi -a | grep off-line > /dev/null; then
-#    else
-#        xsetroot -name "Bat. $( acpi -b | awk '{ print $4 " " $5 }' | tr -d ',' ) | Vol. $(amixer get Master | tail -1 | awk '{ print $5}' | tr -d '[]') | $(date +"%a, %b %d %R")"
-#        xsetroot -name "Vol. $(amixer get Master | tail -1 | awk '{ print $5}' | tr -d '[]') | $(date +"%a, %b %d %R")"
-#    fi
-#    sleep 1s
-# done &
+while true;
+do
+	percentage1=`acpi -b|awk '{print $4}'`
+	bat=`echo "${percentage1//\,}"`
+	battime=`acpi -b|awk '{print $5}'`
+	mem=`free -h|awk 'NR==2{print $3}'`
+	acpi -a | grep "off-line" > /dev/null
+	if [ $? -eq 0 ]; then
+		charging=" [$battime｜$bat]";
+	else
+		charging=" [$battime｜$bat]";
+	fi
+	xsetroot -name "|   $(df -h | awk '{ if ($6 == "/") print $4 }') |   $mem | $charging |   $(amixer get Master|tail -n 1|awk '{print $5}'|tr -d '[]') | $(date '+%Y-%m-%d %R ') "
+	# Update time
+	sleep 1s
+done &
